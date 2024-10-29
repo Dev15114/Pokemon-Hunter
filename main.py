ee428af6fadb4ffa51eaeb5d16069d1f51a8136e
Dev15114 <nontawatdamchawai174@gmail.com>
2024-10-30 00:53:12 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -11,5 +11,6 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 Token = 'MTMwMDc0ODE5ODY3MDg5MzEzNw.GjgkP8.2SR_iDZ-EjaNj6Rlnj4wX65dnGT51EyLAN5aHc'
 
 sever_on()
+
 bot.run(os.getenv('TOKEN'))
 
